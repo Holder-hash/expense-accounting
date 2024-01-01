cc2d30438fdb7b964bc9383cf83dffaf23b5bc0e
Holder-hashu <X-Lamamen-X@yandex.ru>
2024-01-01 17:10:14 +0300

    body & header styles

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.scss';
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <h1>Expense-accounting</h1>
+                <p>Powered by h0ler</p>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
