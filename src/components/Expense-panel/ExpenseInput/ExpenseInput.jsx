ea137e80c163c63b4d0eef1963fb56025446ce6c
Holder-hashu <X-Lamamen-X@yandex.ru>
2024-01-01 20:52:18 +0300

    categories json

--- src/components/Expense-panel/ExpenseInput/ExpenseInput.jsx
+++ src/components/Expense-panel/ExpenseInput/ExpenseInput.jsx
@@ -1,10 +1,26 @@
 import './ExpenseInput.scss';
+import categories from '../../../data/categories.json'
 
 const ExpenseInput = () => {
     return (
         <div className='expanse-input__container'>
             <form>
                 <input type="number" className='expanse-input__expense-inp'/>
+
+                <div className="expanse-input__categories">
+                    {Object.keys(categories).map((key, index) => {
+                        return (
+                            <div key={index}>
+                                <p>{key}: {index}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+
+                <div className='expanse-input__footer'>
+                    <button className='expanse-input__clear-btn'>Очистить</button>
+                    <input type="submit" className='expanse-input__submit-btn' value={'Готово'}/>
+                </div>
             </form>
         </div>
     )
