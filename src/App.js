d760e587bff5432e7f37391f5949b6f643ca2948
Holder-hashu <X-Lamamen-X@yandex.ru>
2024-01-01 17:26:35 +0300

    panel body

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header.jsx';
+import ExpensePanel from './components/Expense-panel/ExpensePanel.jsx';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main>
+      <ExpensePanel />
+      </main>
     </div>
   );
 }
---
+++ src/components/Expense-panel/ExpensePanel.jsx
@@ -0,0 +1,11 @@
+import './Expense-panel.scss';
+
+const ExpensePanel = () => {
+    return (
+        <div className='expanse-panel__body'>
+            <div className='expanse-panel__linse'></div>
+        </div>
+    )
+}
+
+export default ExpensePanel;
